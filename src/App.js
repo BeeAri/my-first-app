c966c41c97ca3aae12e90300a6d7187196fa9842
BeeAri <122098016+BeeAri@users.noreply.github.com>
2023-02-06 17:52:36 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+let username = 'Al';
+const element1 = 
+(
+<h1>Merhaba {username}</h1>
+);
 function App() {
   return (
     <div className="App">
@@ -8,6 +13,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Merhaba
+        </p>
+        <p>
+          {element1}
         </p>
         <a
           className="App-link"
