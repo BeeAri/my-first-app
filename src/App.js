3094782094d5d7e5dc1db512143bb4946c95c9fb
BeeAri <122098016+BeeAri@users.noreply.github.com>
2023-02-07 15:57:51 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
 
-let username = 'Al';
-const element1 = 
-(
-<h1>Merhaba {username}</h1>
-);
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Merhaba
-        </p>
-        <p>
-          {element1}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<div className="App">
+<Routes>
+<Route path="/" element={ <Home/> } />
+<Route path="/about" element={ <About/> } />
+<Route path="/contact" element={ <Contact/> } />
+</Routes>
+</div>
+)
 }
-
-export default App;
+export default App
